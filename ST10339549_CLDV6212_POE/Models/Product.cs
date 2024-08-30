852589d99b7601d535460bb1f7f115f3871a7633
Melissa Govender <melissa.govender@derivco.co.za>
2024-08-30 18:30:47 +0200

    Updated Product Table

--- ST10339549_CLDV6212_POE/Models/Product.cs
+++ ST10339549_CLDV6212_POE/Models/Product.cs
@@ -3,33 +3,17 @@ using System.ComponentModel.DataAnnotations;
 
 namespace ST10339549_CLDV6212_POE.Models
 {
-    public class Product : TableEntity
+  public class Product : TableEntity
+  {
+    [Required]
+    public string? ProductId { get; set; }
+    public string ProductName { get; set; }
+    public string ProductDescription { get; set; }
+    public double ProductPrice { get; set; }
+    public Product()
     {
-        private string _productId;
-
-        [Required]
-        public string? ProductId
-        {
-            get => _productId;
-            set
-            {
-                _productId = value;
-                RowKey = _productId ?? Guid.NewGuid().ToString(); // Ensures RowKey is set when ProductId is set
-            }
-        }
-
-        [Required]
-        public string ProductName { get; set; }
-
-        public string ProductDescription { get; set; }
-
-        [Required]
-        public double ProductPrice { get; set; }
-
-        public Product()
-        {
-            PartitionKey = "Product";
-            RowKey = Guid.NewGuid().ToString(); // Ensures RowKey is initialized
-        }
+      PartitionKey = "Product";
+      RowKey = ProductId ?? Guid.NewGuid().ToString();
     }
+  }
 }
